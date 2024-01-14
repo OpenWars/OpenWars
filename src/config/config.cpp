d4b788e9046c9b39854b4fcdf2f9d0c87703297b
ilaicraftYT <rubencitoperez569@gmail.com>
2024-01-13 21:10:13 -0600

    strcat is trolling me

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -9,19 +9,17 @@ namespace Config {
 char *CONFIG_PATH = nullptr;
 void initialize() {
     if (std::getenv("HOME")) {
-        CONFIG_PATH = strcat(std::getenv("HOME"), "/.config/openwars");
+        strcat(std::getenv("HOME"), "/.config/openwars");
     } else {
-        CONFIG_PATH = strcat(std::getenv("APPDATA"), "/openwars");
+        strcat(std::getenv("APPDATA"), "/openwars");
     }
     // TODO: Mac (or does linux implementation fix it?)
 
     struct stat configPath;
     if (stat(CONFIG_PATH, &configPath) != 0) {
-        std::filesystem::create_directories(CONFIG_PATH);
-
-        std::filesystem::create_directory(strcat(CONFIG_PATH, "/maps"));
-        std::filesystem::create_directory(strcat(CONFIG_PATH, "/content"));
-        std::filesystem::create_directory(strcat(CONFIG_PATH, "/logs"));
+        std::filesystem::create_directories(std::string(CONFIG_PATH) + "/maps");
+        std::filesystem::create_directories(std::string(CONFIG_PATH) + "/content");
+        std::filesystem::create_directories(std::string(CONFIG_PATH) + "/logs");
     }
     // just assume that it isn't created, it could be a file though
 }
