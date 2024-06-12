333875b139c0a2343e16edc7c60113e1c136a79c
Rivest Osz <rivest-oss@proton.me>
2024-06-11 21:15:48 -0300

    WIP Lua scripts class

---
+++ src/game/lua/lua.hpp
@@ -0,0 +1,24 @@
+#ifndef __openwars__game__lua__lua__hpp
+#define __openwars__game__lua__lua__hpp
+
+#define SOL_ALL_SAFETIES_ON 1
+#include <sol/sol.hpp>
+
+namespace OpenWars {
+	class LuaState {
+		private:
+			const char *err_str = nullptr;
+			sol::state sol_state;
+
+		public:
+			LuaState(void);
+			~LuaState(void);
+
+			const char *get_error(void);
+			int load_script(const char *path);
+			int unload_script(void);
+			int run_script(void);
+	};
+};
+
+#endif
