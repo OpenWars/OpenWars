6a6b480a77f82ec0a63eeca52e84e5ac95444f3b
ilacraftYT <rubencitoperez569@gmail.com>
2024-06-15 14:52:03 -0600

    chore(scene): fix naming and add controller

---
+++ src/game/scene/scene_controller.cpp
@@ -0,0 +1,8 @@
+#ifndef __openwars__scene__scene_controller__cxx__
+#define __openwars__scene__scene_controller__cxx__
+
+#include "scene.hpp"
+
+OpenWars::SceneController::SceneController(){}
+
+#endif
