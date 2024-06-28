433a509950c29b474380ba3f6b8739b6720cf4c2
Rivest Osz <rivest-oss@proton.me>
2024-06-27 22:28:49 -0300

    Separating ErrorOr

--- src/defs.hpp
+++ src/defs.hpp
@@ -16,6 +16,86 @@ namespace OpenWars {
 	
 	typedef float f32;
 	typedef double f64;
+
+	typedef struct { const char *error; } Error;
+	
+	template<typename Type>
+	class ErrorOr {
+		private:
+			Type val;
+		
+		public:
+			const char *error = nullptr;
+			
+			ErrorOr(void) {
+				error = nullptr;
+			};
+			
+			ErrorOr(Error error_struct) {
+				error = error_struct.error;
+			};
+			
+			ErrorOr(Type value_pointer) {
+				error = nullptr;
+				val = value_pointer;
+			};
+			
+			Type value(void) { return val; };
+	};
+	
+	template<typename Type>
+	class ErrorOr<Type*> {
+		private:
+			Type *val = nullptr;
+		
+		public:
+			const char *error = nullptr;
+			
+			ErrorOr(void) {
+				error = nullptr;
+			};
+			
+			ErrorOr(Error error_struct) {
+				error = error_struct.error;
+			};
+			
+			ErrorOr(Type *value_pointer) {
+				error = nullptr;
+				val = value_pointer;
+			};
+			
+			Type *value(void) { return val; };
+	};
+	
+	template<typename Type>
+	class ErrorOr<Type&> {
+		public:
+			const char *error = nullptr;
+			Type* value_pointer = nullptr;
+			
+			ErrorOr(void) {
+				error = nullptr;
+			};
+			
+			ErrorOr(Error error_struct) {
+				error = error_struct.error;
+			};
+			
+			ErrorOr(Type *val_ptr) {
+				error = nullptr;
+				value_pointer = val_ptr;
+			};
+			
+			Type& value(void) { return (*value_pointer); };
+	};
+	
+	template<>
+	class ErrorOr<void> {
+		public:
+			const char *error = nullptr;
+			
+			ErrorOr(Error error_struct) { error = error_struct.error; };
+	};
 };
 
 #endif
