0684ba403c1eb9b5313456478b6983382c2059c6
Rivest Osz <rivest-oss@proton.me>
2024-07-04 20:45:34 -0300

    I know this sucks but hey!

--- src/codec/media-player.cpp
+++ src/codec/media-player.cpp
@@ -196,7 +196,7 @@ namespace OpenWars {
 		int e;
 
 		AVPacket packet;
-		while(av_read_frame(mp->fmt_ctx, &packet) >= 0) {
+		while((e = av_read_frame(mp->fmt_ctx, &packet)) >= 0) {
 			if(packet.stream_index == mp->str_video->index) {
 				e = avcodec_send_packet(mp->codec_ctx_video, &packet);
 				if(e < 0) {
@@ -208,22 +208,18 @@ namespace OpenWars {
 					return Error { "Couldn't send next frame request" };
 				}
 
-				while(true) {
-					e = avcodec_receive_frame(mp->codec_ctx_video, mp->av_frame);
+				while((e = avcodec_receive_frame(mp->codec_ctx_video, mp->av_frame)) == 0) {
+					return Error { nullptr };
 
-					if(e < 0) {
-						close();
-
-						if(av_strerror(e, err_buff, 128) == 0)
-							return Error { err_buff };
-
-						return Error { "Couldn't receive next frame" };
-					}
+					av_packet_unref(&packet);
+					return Error { nullptr };
 				};
 			}
 
 			av_packet_unref(&packet);
 		};
+
+		return Error { nullptr };
 	};
 
 	ErrorOr<MediaPlayer::video_frame_t *> MediaPlayer::get_frame(void) {
