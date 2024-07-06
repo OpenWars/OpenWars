e6a8263907d327a3da576d0d240b5220fc6d1a8c
Rivest Osz <rivest-oss@proton.me>
2024-07-06 14:32:41 -0300

    Adding Audio files

---
+++ src/sound/sound.cpp
@@ -0,0 +1,10 @@
+#ifndef __openwars__game__sound__sound__cpp__
+#define __openwars__game__sound__sound__cpp__
+
+#include "sound.hpp"
+
+namespace OpenWars {
+	//
+};
+
+#endif
---
+++ src/sound/sound.hpp
@@ -0,0 +1,10 @@
+#ifndef __openwars__game__sound__sound__hpp__
+#define __openwars__game__sound__sound__hpp__
+
+#include "../defs.hpp"
+
+namespace OpenWars {
+	typedef struct {} audio_stream_t;
+};
+
+#endif
