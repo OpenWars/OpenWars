2fff930d649fc8d68ad8aeed4f0c123b37dfbc1f
Rivest Osz <rivest-oss@proton.me>
2024-07-09 20:41:05 -0300

    Adding sound struct and functions

--- src/sound/sound.hpp
+++ src/sound/sound.hpp
@@ -4,7 +4,17 @@
 #include "../defs.hpp"
 
 namespace OpenWars {
-	typedef struct {} audio_stream_t;
+	typedef struct {
+		u64			samples;
+		// (-1.0)...(1.0) samples.
+		float	*data;
+
+		uintptr_t	data_ptr;
+	} sound_t;
+
+	ErrorOr<sound_t *> load_sound_from_file(const char *filepath);
+	ErrorOr<void> update_sound(sound_t *s);
+	void free_sound(sound_t *s);
 };
 
 #endif
