d25b51396379d4563ee678b7a21c9049ffe17dde
Rivest Osz <rivest-oss@proton.me>
2024-07-11 02:36:39 -0300

    A warning less

--- src/io/log.cpp
+++ src/io/log.cpp
@@ -6,13 +6,12 @@
 #include <cstdio>
 
 namespace OpenWars {
-	const char	*_TEXT_LOG = "\x1b[93m[\x1b[91mOpen\x1b[31mWars\x1b[93m]\x1b[0m",
-				*_TEXT_DEBUG_LOG = "\x1b[96m[\x1b[95mDEBUG\x1b[96m]\x1b[0m ",
+	const char	*_TEXT_DEBUG_LOG = "\x1b[96m[\x1b[95mDEBUG\x1b[96m]\x1b[0m ",
 				*_TEXT_INFO_LOG = "\x1b[93m[\x1b[33mINFO\x1b[93m]\x1b[0m ",
 				*_TEXT_ERROR_LOG = "\x1b[97m[\x1b[31mERROR\x1b[97m]\x1b[0m ";
 	
 	int log_something(std::FILE *file, const char *pre, const char *format, std::va_list args) {
-		(void)std::fprintf(file, _TEXT_LOG);
+		(void)std::fprintf(file, "\x1b[93m[\x1b[91mOpen\x1b[31mWars\x1b[93m]\x1b[0m");
 		(void)std::fprintf(file, pre);
 		int res = std::vfprintf(file, format, args);
 
