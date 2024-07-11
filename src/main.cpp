5b73bcbe52976895ec2329698e86db9ec81bd76c
Rivest Osz <rivest-oss@proton.me>
2024-07-11 02:43:08 -0300

    Adding the Auditor to main

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,52 @@
 #include "config/config.hpp"
 #include "logic/task/task.hpp"
 #include "visual/visual.hpp"
+#include "misc/auditor.hpp"
+#include "io/log.hpp"
 
 #include <cstdio>
 
+using namespace OpenWars;
+
 int main(void) {
+	ErrorOr<void> ve = init_auditor();
+	if(ve.error) {
+		log_error("Couldn't initialize the auditor: %s\n", ve.error);
+		return 1;
+	}
+
 	const int screenWidth = 800;
 	const int screenHeight = 450;
 
-	OpenWars::SceneController sceneController;
-	OpenWars::Config config;
+	SceneController sceneController;
+	Config config;
 
 	if(config.load() < 0) {
-		std::fprintf(stderr, "{CONFIG_LOAD_ERROR} %s\n", config.get_error());
+		log_error("{CONFIG_LOAD_ERROR} %s\n", config.get_error());
 		return 1;
 	}
 
-	OpenWars::Tasks::King tasks_king;
+	Tasks::King tasks_king;
 
 	if(tasks_king.init_pawns() < 0) {
-		std::fprintf(stderr, "{TASK_SCHEDULER_CREATE_ERROR} %s\n", tasks_king.get_error());
+		log_error("{TASK_SCHEDULER_CREATE_ERROR} %s\n", tasks_king.get_error());
 		return 1;
 	}
 
-	OpenWars::init_video(screenWidth, screenHeight, "OpenWars");
+	init_video(screenWidth, screenHeight, "OpenWars");
 
-	while(OpenWars::should_close_window() == false) {
-		OpenWars::init_frame();
+	while(should_close_window() == false) {
+		init_frame();
 
 		// [TODO]
 
-		OpenWars::swap_buffers();
+		swap_buffers();
 	};
 
-	OpenWars::deinit_video();
+	deinit_video();
 	tasks_king.deinit_pawns();
+
+	deinit_auditor();
 
 	return 0;
 }
