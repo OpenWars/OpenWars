dd33c4bb4579f75445be84ba4b0b2b0ffc418ce5
Rivest Osz <rivest-oss@proton.me>
2024-07-14 20:13:07 -0300

    First GOAP thingy

--- src/logic/ai/goap.hpp
+++ src/logic/ai/goap.hpp
@@ -4,6 +4,34 @@
 #include "../../defs.hpp"
 
 namespace OpenWars {
+	namespace GOAP {
+		typedef uintptr_t state_t;
+
+		typedef u32 (*cost_t)(state_t state);
+		typedef bool (*condition_t)(state_t state);
+		typedef state_t (*reaction_t)(state_t state);
+
+		typedef struct {
+			condition_t	condition;
+			reaction_t	react;
+			cost_t		cost;
+		} action_t;
+
+		typedef struct {
+			u32			cost;
+			u32			length;
+			action_t	*actions;
+		} plan_t;
+
+		class Planner {
+			public:
+				void init(void);
+				void deinit(void);
+
+				void add_action(action_t action);
+				plan_t *get_plan(void);
+		};
+	};
 };
 
 #endif
