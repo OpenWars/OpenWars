ef160e4abb1127b6158051c30b179df20550a7f5
Rivest Osz <rivest-oss@proton.me>
2024-07-24 16:50:44 -0300

    Thingy

--- src/misc/localization.hpp
+++ src/misc/localization.hpp
@@ -9,13 +9,11 @@ namespace OpenWars {
 	namespace L10N {
 		typedef std::unordered_map<const char *, const char *> info_t;
 
-		extern void *s_lang;
-
 		ErrorOr<void> init_localization(void);
 		void deinit_localization(void);
 
 		ErrorOr<void> load_language(const char *s);
-		const char *get_text(u32 id, info_t &info);
+		char *get_text(u32 id, info_t &info);
 	};
 };
 
