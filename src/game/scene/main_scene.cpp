9d68d6a0337dd5d5395c9c42647eb9514d4a0e3b
ilaicraftYT <soyilai@proton.me>
2024-08-01 18:00:55 -0600

    fix LICENSE: switch to AGPL

--- src/game/scene/main_scene.cpp
+++ src/game/scene/main_scene.cpp
@@ -1,3 +1,28 @@
+/**
+ *
+   ___                __        __
+  / _ \ _ __   ___ _ _\ \      / /_ _ _ __ ___
+ | | | | '_ \ / _ \ '_ \ \ /\ / / _` | '__/ __|
+ | |_| | |_) |  __/ | | \ V  V / (_| | |  \__ \
+  \___/| .__/ \___|_| |_|\_/\_/ \__,_|_|  |___/
+       |_|
+
+Copyright (C) 2024 OpenWars Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef __openwars__game__scene__main_scene__cxx__
 #define __openwars__game__scene__main_scene__cxx__
 
