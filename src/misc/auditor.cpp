ef57fca9ad57bccab6ef5a717831ca9322c14cc7
Rivest Osz <rivest-oss@proton.me>
2024-08-02 21:18:20 -0300

    Adding 128-bit hashing for Auditor

--- src/misc/auditor.cpp
+++ src/misc/auditor.cpp
@@ -37,17 +37,94 @@ namespace OpenWars {
 		rnd.init();
 
 		u32 h = 0xffffffff;
+		u32 len = 0;
 		
 		while(*s != '\0') {
 			rnd.seed((u32)(*s));
 			h <<= 1;
 			h ^= rnd.random_u32();
 			s++;
+
+			len++;
 		};
 
+		h ^= len;
 		h ^= rnd.random_u32();
 
 		return h;
+	};
+
+	struct p_audit_h128_t {
+		u32	b0, b1, b2, b3;
+
+		bool operator==(p_audit_h128_t &other) {
+			return (
+				(b0 == other.b0) &
+				(b1 == other.b1) &
+				(b2 == other.b2) &
+				(b3 == other.b3)
+			);
+		};
+	};
+
+	p_audit_h128_t p_audits_hash128(const char *s, p_audit_h128_t c) {
+		p_audit_h128_t h = {
+			3141592653,
+			589793238,
+			462643383,
+			2795028841,
+		};
+
+		h.b0 ^= c.b0;
+		h.b1 ^= c.b1;
+		h.b2 ^= c.b2;
+		h.b3 ^= c.b3;
+
+		Random rnd;
+		rnd.init();
+
+		u32 len = 0;
+		
+		while(*s != '\0') {
+			rnd.seed((u32)(*s));
+
+			h.b0 <<= 1;
+			h.b0 ^= rnd.random_u32();
+
+			h.b1 <<= 3;
+			h.b1 ^= rnd.random_u32();
+
+			h.b2 <<= 5;
+			h.b2 ^= h.b0;
+			h.b2 ^= rnd.random_u32();
+
+			h.b3 <<= 7;
+			h.b3 ^= h.b1;
+			h.b3 ^= rnd.random_u32();
+
+			s++;
+			len++;
+		};
+
+		h.b0 ^= len;
+		h.b1 += len;
+		h.b2 -= len;
+		h.b3 ^= len;
+
+		rnd.seed(h.b3);
+		h.b0 ^= rnd.random_u32();
+		rnd.seed(h.b2);
+		h.b1 ^= rnd.random_u32();
+		rnd.seed(h.b1);
+		h.b2 ^= rnd.random_u32();
+		rnd.seed(h.b0);
+		h.b3 ^= rnd.random_u32();
+
+		return h;
+	};
+
+	p_audit_h128_t p_audits_hash128(const char *s) {
+		return p_audits_hash128(s, { 3, 5, 7, 11 });
 	};
 
 	typedef struct {
