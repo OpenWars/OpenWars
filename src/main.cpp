293361121041795ee3d57e11395bb64a97e05266
Rivest Osz <rivest-oss@proton.me>
2024-11-14 00:52:24 -0300

    Test ClayUI?

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "logic/task/task.hpp"
 #include "misc/auditor.hpp"
 #include "visual/visual.hpp"
+#include "io/clay.hpp"
 #include <cstdlib>
 
 const int screen_width = 1280;
@@ -10,11 +11,16 @@ const int screen_height = 720;
 OpenWars::Tasks::King king;
 
 void ciao(int code) {
+	OpenWars::Clay::deinit();
 	OpenWars::deinit_video();
 	king.deinit_pawns();
 	OpenWars::deinit_auditor();
 
 	std::exit(code);
+};
+
+namespace Raylib {
+	#include <raylib.h>
 };
 
 int main(void) {
@@ -35,9 +41,29 @@ int main(void) {
 		ciao(1);
 	}
 
+	if(OpenWars::Clay::init(&err) < 0) {
+		OpenWars::log_error("Couldn't initialize ClayUI: %s\n", err);
+		ciao(1);
+	}
+
+	Raylib::Camera2D cam;
+	cam.target = Raylib::Vector2 { 0.0f, 0.0f };
+	cam.offset = Raylib::Vector2 { 0.0f, 0.0f };
+	cam.rotation = 0.0f;
+	cam.zoom = 1.0f;
+
+	Clay_LayoutConfig layoutElement = Clay_LayoutConfig { .padding = {5} };
+
 	while(OpenWars::should_close_window() == false) {
 		OpenWars::init_frame(&err);
 
+		// [TODO]
+		OpenWars::Clay::update();
+		OpenWars::Clay::start_frame();
+
+		// [TODO]
+
+		OpenWars::Clay::end_frame(&cam);
 		// [TODO]
 
 		OpenWars::swap_buffers(&err);
