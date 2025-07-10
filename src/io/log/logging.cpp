a5b24ecb6c6ef9403897077520e6aedc7cb3bd43
Rivest Osz <rivest-oss@proton.me>
2025-07-10 12:04:18 -0300

    Working logging & no warns anymore

--- src/io/log/logging.cpp
+++ src/io/log/logging.cpp
@@ -1,10 +1,19 @@
+#include <ctime>
+#include <cstdio>
 #include "logging.hpp"
+#include "../graphics/graphics.hpp"
+
+namespace OpenWars::IO::Logging {
+    const char *_TEXT_DEBUG_LOG = "\x1b[96m[\x1b[95mDEBUG\x1b[96m]\x1b[0m ",
+        *_TEXT_INFO_LOG = "\x1b[93m[\x1b[33mINFO\x1b[93m]\x1b[0m ",
+        *_TEXT_ERROR_LOG = "\x1b[97m[\x1b[31mERROR\x1b[97m]\x1b[0m ";
+};
 
 void OpenWars::IO::Logging::init(){
-    //raylib::SetTraceLogCallback(OpenWars::IO::Logging::out);
+    raylib::SetTraceLogCallback(OpenWars::IO::Logging::out);
 }
-/*
-void OpenWars::IO::Logging::out(int type, const char *message, va_list args){
+
+void OpenWars::IO::Logging::out(int type, const char *message, std::va_list args){
     char timeStr[64] = { 0 };
     time_t now = time(NULL);
     struct tm *tm_info = localtime(&now);
@@ -23,4 +32,4 @@ void OpenWars::IO::Logging::out(int type, const char *message, va_list args){
 
     vprintf(message, args);
     printf("\n");
-}*/
+}
--- src/io/log/logging.hpp
+++ src/io/log/logging.hpp
@@ -1,14 +1,11 @@
 #pragma once
+#include <cstdarg>
 
 namespace OpenWars {
     namespace IO {
         namespace Logging {
-            const static char	*_TEXT_DEBUG_LOG = "\x1b[96m[\x1b[95mDEBUG\x1b[96m]\x1b[0m ",
-			                    *_TEXT_INFO_LOG = "\x1b[93m[\x1b[33mINFO\x1b[93m]\x1b[0m ",
-			                    *_TEXT_ERROR_LOG = "\x1b[97m[\x1b[31mERROR\x1b[97m]\x1b[0m ";
-
             void init();
-            //void out(int type, const char *message, va_list args);
+            void out(int type, const char *message, std::va_list args);
         }
     }
 }
