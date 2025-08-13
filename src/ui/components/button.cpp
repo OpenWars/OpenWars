8522a20666bdcc9ccc71a6c271d3867a69bddfdb
ilaicraftYT <soyilai@proton.me>
2025-08-12 22:27:52 -0600

    button hover

--- src/ui/components/button.cpp
+++ src/ui/components/button.cpp
@@ -5,24 +5,29 @@
 void OpenWars::UI::ButtonComponent::render() {
     int skew = Theme::SKEW / 3.f;
 
-    raylib::Vector2 shadowPos = {position.x + 2, position.y + 2};
-    Utils::Drawing::drawParallelogram(shadowPos, width, height, skew,
-                                      Colors::ZINC_600);
+    raylib::Color bg =
+        isHovered ? raylib::ColorBrightness(background, 0.2) : background;
+    raylib::Color outline = isHovered
+                                ? raylib::ColorBrightness(Colors::ZINC_500, 0.2)
+                                : Colors::ZINC_500;
+    raylib::Color shadow = isHovered
+                               ? raylib::ColorBrightness(Colors::ZINC_600, 0.2)
+                               : Colors::ZINC_600;
 
-    Utils::Drawing::drawParallelogram(position, width, height, skew,
-                                      background);
+    raylib::Vector2 shadowPos = {position.x + 2, position.y + 2};
+    Utils::Drawing::drawParallelogram(shadowPos, width, height, skew, shadow);
+
+    Utils::Drawing::drawParallelogram(position, width, height, skew, bg);
     Utils::Drawing::drawParallelogramOutline(position, width, height, skew,
-                                             Colors::ZINC_500, 1.5f);
+                                             outline, 1.5f);
 
     float centerX = position.x + (width + skew) / 2.0f;
     float centerY = position.y - height / 2.0f;
 
     int fontSize = 14;
     int textWidth = raylib::MeasureText(label.c_str(), fontSize);
-    raylib::Vector2 labelPos = {
-        centerX - textWidth / 2.0f,
-        centerY - fontSize / 2.0f
-    };
+    raylib::Vector2 labelPos = {centerX - textWidth / 2.0f,
+                                centerY - fontSize / 2.0f};
     raylib::DrawText(label.c_str(), labelPos.x, labelPos.y, fontSize,
                      foreground);
 }
@@ -30,17 +35,13 @@ void OpenWars::UI::ButtonComponent::render() {
 bool OpenWars::UI::ButtonComponent::handleInput(
     const IO::Input::InputState &state) {
     float skew = Theme::SKEW / 3.f;
-    
-    raylib::Rectangle bounds = {
-        position.x,
-        position.y - height,
-        width + skew,
-        height
-    };
-    
+
+    raylib::Rectangle bounds = {position.x, position.y - height, width + skew,
+                                height};
+
     isHovered = raylib::CheckCollisionPointRec(state.mousePos, bounds);
 
-    if (isHovered && state.pressingLeft){
+    if (isHovered && state.pressingLeft) {
         this->parent->handleButtonInput(this->id);
         return true;
     }
